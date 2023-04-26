d3f978c76b743587509f2531a28fe0bbd9d325d7
Lucas Montero Pedrosa <57368590+Lucas-Montero-42@users.noreply.github.com>
2023-04-26 20:18:29 +0200

    Cambios

--- Assets/Scripts/FireExtinguish.cs
+++ Assets/Scripts/FireExtinguish.cs
@@ -5,17 +5,10 @@ using UnityEngine.InputSystem;
 
 public class FireExtinguish : MonoBehaviour
 {
-    Vector3 mouseScreenPos;
-    Vector3 mosuseWorldPos;
-    public LayerMask GroundLayer;
     public LayerMask FireLayer;
     GameObject Player;
     public float WeakRayLenght;
-    bool WeakHittingFire;
     public float StrongRayLenght;
-    bool StrongHittingFire;
-    public GameObject WeakFireTarget;
-    public GameObject StrongFireTarget;
     [SerializeField] private InputPlayerController playerInput;
     PickupKid Kid;
     Manguera Manguera;
@@ -23,8 +16,6 @@ public class FireExtinguish : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        WeakHittingFire = false;
-        StrongHittingFire = false;
         Player = GameObject.FindGameObjectWithTag("Player");
         playerInput = Player.GetComponent<InputPlayerController>();
         Kid = Player.GetComponent<PickupKid>();
@@ -34,7 +25,6 @@ public class FireExtinguish : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PositionMouse();
         if (playerInput.shoot && !Manguera.GetSecondary() && Manguera.GetWaterAmount() > 0)
         {
             WeakWaterRaycast();
@@ -52,12 +42,8 @@ public class FireExtinguish : MonoBehaviour
         Debug.DrawRay(Player.transform.position, Player.transform.forward* WeakRayLenght);
         if (Physics.Raycast(ray, out RaycastHit hit, WeakRayLenght, FireLayer))
         {
-            WeakHittingFire = true;
-            WeakFireTarget = hit.collider.gameObject;
             Debug.Log("normal");
         }
-        else
-            WeakHittingFire = false;        
     }
     private void StrongWaterRaycast()
     {
@@ -65,23 +51,7 @@ public class FireExtinguish : MonoBehaviour
         Debug.DrawRay(Player.transform.position, Player.transform.forward * StrongRayLenght);
         if (Physics.Raycast(ray, out RaycastHit hit, StrongRayLenght, FireLayer))
         {
-            StrongHittingFire = true;
-            StrongFireTarget = hit.collider.gameObject;
             Debug.Log("fuerte");
         }
-        else
-            StrongHittingFire = false;
-    }
-    private void PositionMouse()
-    {
-        mouseScreenPos = Mouse.current.position.ReadValue();
-        Ray ray = Camera.main.ScreenPointToRay(mouseScreenPos);
-
-        if (Physics.Raycast(ray, out RaycastHit hit, 1000, GroundLayer))
-        {
-            mosuseWorldPos = hit.point;
-        }
-
-        transform.position = mosuseWorldPos;
     }
 }
