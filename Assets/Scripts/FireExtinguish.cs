23b33757776be016882ba7e8bc628cbee1f22c88
Lucas Montero Pedrosa <57368590+Lucas-Montero-42@users.noreply.github.com>
2023-04-26 10:57:05 +0200

    DEah ray

--- Assets/Scripts/FireExtinguish.cs
+++ Assets/Scripts/FireExtinguish.cs
@@ -5,8 +5,10 @@ using UnityEngine.InputSystem;
 
 public class FireExtinguish : MonoBehaviour
 {
+    Vector3 mouseScreenPos;
+    Vector3 mosuseWorldPos;
+    public LayerMask LM;
     private GameObject Player;
-    public GameObject Pointer;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +20,24 @@ public class FireExtinguish : MonoBehaviour
     void Update()
     {
         WaterRaycast();
+        PositionMouse();
     }
 
     private void WaterRaycast()
     {
         Ray ray = new Ray(Player.transform.position, Player.transform.forward);
         Debug.DrawRay(Player.transform.position, Player.transform.forward*5);
+    }
+    private void PositionMouse()
+    {
+        mouseScreenPos = Mouse.current.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(mouseScreenPos);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 1000, LM))
+        {
+            mosuseWorldPos = hit.point;
+        }
+
+        transform.position = mosuseWorldPos;
     }
 }
