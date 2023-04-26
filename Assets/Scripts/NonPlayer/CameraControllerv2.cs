8d8c70174b3b63de352f9fe45d7441359bc652b3
IsaacMartos <77277945+IsaacMartos@users.noreply.github.com>
2023-04-27 00:08:01 +0200

    zoom

--- Assets/Scripts/NonPlayer/CameraControllerv2.cs
+++ Assets/Scripts/NonPlayer/CameraControllerv2.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraControllerv2 : MonoBehaviour
 {
     public Transform target;
     public float distance = 10f;
-    
+    private InputPlayerController controller;
+    private Camera cam;
+
+    public float maxCamSize;
+    public float minCamSize;
+
+    [SerializeField] float timer;
+    float delay = 5f;
+
+    private void Start()
+    {
+        controller = GetComponent<InputPlayerController>();
+        cam = Camera.main;
+        timer = delay;
+    }
 
     private void Update()
     {
@@ -15,5 +30,37 @@ public class CameraControllerv2 : MonoBehaviour
             transform.position = target.position;
             transform.LookAt(target);
         }
+
+        if (controller.zoom.y < 0)
+        {
+            timer = delay;
+            cam.orthographicSize += 1f;
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, minCamSize, maxCamSize);
+        }
+
+        if (controller.zoom.y > 0)
+        {
+            timer = delay;
+            cam.orthographicSize -= 1f;
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, minCamSize, maxCamSize);
+        }
+
+        else if(controller.zoom.y == 0)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        if (timer <= 0)
+        {
+            StartCoroutine(ResetCamera());  
+        }
+        
+    }
+
+    IEnumerator ResetCamera()
+    {
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 13f, 2f * Time.deltaTime);        
+        yield return new WaitForSeconds(2.5f);
+        timer = delay;
     }
 }
