e291744c29fa68f976896ff8442aa53883abc1ab
Lucas Montero Pedrosa <57368590+Lucas-Montero-42@users.noreply.github.com>
2023-05-11 23:09:39 +0200

    Camera change

---
+++ Assets/Scripts/Cameras/CamPreviewManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamPreviewManager : MonoBehaviour
+{
+    public Camera[] cameraList;
+    public Camera playerCam;
+    public float timeOnEachCamera;
+    int currentCam = 0;
+
+    //When the level begins all the cameras are disabled
+    //The cameras get enabeled in order and they stay enabeled the number of seconds definied above, using a recursive corroutine
+    //Finlaly when all the cameras have been shown the player camera is the only one enabeled
+   
+    void Start()
+    {
+        playerCam.enabled = false;
+        for (int i = 0; i < cameraList.Length; i++)
+        {
+            cameraList[i].enabled = false;
+        }
+        StartCoroutine(Cameras());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator Cameras()
+    {
+        cameraList[currentCam].enabled = true;
+        yield return new WaitForSeconds(timeOnEachCamera);
+        currentCam++;
+        if (currentCam < cameraList.Length)
+        {
+            cameraList[currentCam].enabled = false;
+            StartCoroutine(Cameras());
+        }
+        else
+        {
+            playerCam.enabled = true;
+        }
+    }
+}
