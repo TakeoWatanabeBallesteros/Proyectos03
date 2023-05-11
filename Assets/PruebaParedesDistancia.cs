2545ba0b40fc843097aef04acdef1eb90e6d0d9b
Lucas Montero Pedrosa <57368590+Lucas-Montero-42@users.noreply.github.com>
2023-05-12 01:41:16 +0200

    a

--- Assets/PruebaParedesDistancia.cs
+++ Assets/PruebaParedesDistancia.cs
@@ -10,13 +10,13 @@ public class PruebaParedesDistancia : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        WallHeight = Paredes[0].GetComponent<MeshRenderer>().material.GetFloat("_DisapearR");
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        WallHeight = Paredes[0].GetComponent<MeshRenderer>().material.GetFloat("_DisapearR");
     }
     private void OnTriggerEnter(Collider other)
     {
