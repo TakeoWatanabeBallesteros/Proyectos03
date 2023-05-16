14798bb5b537e844936294a3b07707e363a5dc1b
Takeo Watanabe <takeowatanabe3@gmail.com>
2023-05-16 23:53:34 +0200

    fmod

---
+++ Assets/Scripts/UI/FSM_UIManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FSM;
+
+public class FSM_UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
