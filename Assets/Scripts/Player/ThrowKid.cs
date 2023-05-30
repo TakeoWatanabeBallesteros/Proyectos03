462f32eb0a4c2419d9bc93472c5d45428f69a0ac
Lucas Montero Pedrosa <57368590+Lucas-Montero-42@users.noreply.github.com>
2023-05-31 00:51:58 +0200

    Niño goes Yeet

--- Assets/Scripts/Player/ThrowKid.cs
+++ Assets/Scripts/Player/ThrowKid.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 
 public class ThrowKid : MonoBehaviour
 {
+    public GameObject MyBalls;
+    InputPlayerController Input;
+    public Transform Hombro;
+    public float Fuerza = 1f;
+    const float MaxFuerza = 900f;
+    public bool Holding = false;
+    public float FuerzaPerSecond;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Input = GetComponent<InputPlayerController>();
+        Fuerza = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Holding && !Input.secondaryShoot)
+        {
+            var ball = Instantiate(MyBalls);
+            ball.transform.position = Hombro.transform.position;
+            ball.GetComponent<Rigidbody>().AddForce(transform.forward.normalized * Fuerza, ForceMode.Impulse);
+        }
+        Holding = Input.secondaryShoot;
+        if (Holding) Fuerza = Mathf.Clamp(Fuerza += FuerzaPerSecond * Time.deltaTime, 0, MaxFuerza);
         
+        else Fuerza = 0;
+
+
     }
 }
