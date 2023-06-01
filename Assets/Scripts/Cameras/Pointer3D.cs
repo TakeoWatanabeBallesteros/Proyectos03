ae7901953a53610f3ac8a1e5d44e4cb95ad45339
IsaacMartos <77277945+IsaacMartos@users.noreply.github.com>
2023-06-01 19:34:55 +0200

    a

---
+++ Assets/Scripts/Cameras/Pointer3D.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer3D : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;       
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out RaycastHit raycastHit))
+        {
+            transform.position = raycastHit.point;
+        }
+        
+    }
+    
+}
