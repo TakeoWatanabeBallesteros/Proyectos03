dd291746bb79cb4165d66d4b0697fbe34f16d372
Lucas Montero Pedrosa <57368590+Lucas-Montero-42@users.noreply.github.com>
2023-06-01 21:43:00 +0200

    ñlkjhgfd

--- Assets/Scripts/Fire/DamagePlayer.cs
+++ Assets/Scripts/Fire/DamagePlayer.cs
@@ -6,47 +6,33 @@ public class DamagePlayer : MonoBehaviour
 {
     private PlayerHealth PH;
     private bool OnDamageZone;
-    private bool Valve;
-    private float Timer;
 
     // Start is called before the first frame update
     void Start()
     {
         OnDamageZone = false;
-        Valve = true;
-        PH = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
-        Timer = 0;
+        PH = Singleton.Instance.Player.GetComponent<PlayerHealth>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Timer > 0)
-        {
-            Timer -= Time.deltaTime;
-        }
-        else if(OnDamageZone && !PH.isTakingDamage)
+
+        if(OnDamageZone && !PH.isTakingDamage)
         {
             PH.TakeDamage();
-            Timer = 1f;
+            Debug.Log("AU!");
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag ==  "Player")
-        {
-            if (Valve)
-            {
-                Valve = false;
-                Timer = 0;
-            }
-            OnDamageZone = true;
-        }
+        if (other.CompareTag("Player")) OnDamageZone = true;
+        Debug.Log("IN");
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
-            OnDamageZone = false;
+        if (other.CompareTag("Player")) OnDamageZone = false;
+        Debug.Log("OUT");
     }
 }
