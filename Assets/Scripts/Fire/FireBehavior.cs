2f1c6e7edee7367c40fa17f488b8e3b1ba9588dd
Martí Parladé Salvans <mparlade@edu.tecnocampus.cat>
2023-06-11 13:37:17 +0200

    canvis errors

--- Assets/Scripts/Fire/FireBehavior.cs
+++ Assets/Scripts/Fire/FireBehavior.cs
@@ -109,12 +109,15 @@ public class FireBehavior : MonoBehaviour
                 break;
             case "Player":
                 playerHealth.isTakingDamage = false;
-                playerHealth = null;
+               // playerHealth = null;
                 break;
             case "Kid":
-                var children = other.GetComponent<ChildrenHealthSystem>();
-                children.StopBeingBurned();
-                childrens.Remove(children);
+                if (other.GetComponent<ChildrenHealthSystem>())
+                { 
+                    var children = other.GetComponent<ChildrenHealthSystem>();
+                    children.StopBeingBurned();
+                    childrens.Remove(children);
+                }
                 break;
         }
     }
@@ -215,8 +218,11 @@ public class FireBehavior : MonoBehaviour
         if(!childrens.Any()) return;
         foreach (var children in childrens)
         {
+            if (!children.GetComponent<ChildrenHealthSystem>()) return;
+                
             if (Vector3.Distance(children.transform.position, transform.position) > damageRadius) continue;
             children.TakeDamage();
+            
         }
     }
 
