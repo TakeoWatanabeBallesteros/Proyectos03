0d10dfea642bce3ff2f281735d34082394f8d823
Lucas Montero Pedrosa <57368590+Lucas-Montero-42@users.noreply.github.com>
2023-06-15 15:41:33 +0200

    kiujyhg

--- Assets/Scripts/UI/Blackboard_UIManager.cs
+++ Assets/Scripts/UI/Blackboard_UIManager.cs
@@ -157,7 +157,10 @@ public class Blackboard_UIManager : DynamicBlackboard
     {
         GameObject pref = Instantiate(PointsPrefab, PontPopUpOrigin.transform); 
         pref.GetComponent<TextMeshProUGUI>().text = points < 0 ?  "" + points : "+" + points;
-        pref.transform.position = PontPopUpOrigin.transform.position;
+        Vector3 a = new Vector3(PontPopUpOrigin.transform.position.x, PontPopUpOrigin.transform.position.y, PontPopUpOrigin.transform.position.z);
+        a.x += Random.Range(-40,40);
+        a.y += Random.Range(-40, 40);
+        pref.transform.position = a;
         Destroy(pref, pref.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length - 0.1f);
 
     }
